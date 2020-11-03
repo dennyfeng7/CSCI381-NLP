1029c9e91b0762e72c8d5c117b3a8c2f40303c19
dennyfeng7 <denny.feng39@qmail.cuny.edu>
2020-11-03 01:09:54 -0500

    added train/text files

---
+++ MovieReviewClassification_NaiveBayes/NB.py
@@ -0,0 +1,14 @@
+# def naive_bayes(training, test, parameters, output):
+#     # The last line in the output file should list the overall accuracy of the classifier
+#     # on the test data
+
+def main():
+    d = dict()
+    d = {'hi': {'test', 'test2'},
+         'test': {'test', 'no'}
+         }
+    print(d)
+
+
+if __name__ == '__main__':
+    main()
